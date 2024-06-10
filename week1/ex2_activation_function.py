cde09d510a0116b1e3780515c73b0c1284b55fc6
HaoGau <nguyennhuhao219@gmail.com>
2024-06-10 21:06:09 +0700

    update ex 1, 2, 3, 4, 5

--- week1/ex2_activation_function.py
+++ week1/ex2_activation_function.py
@@ -3,33 +3,28 @@ import math
 
 def activation_functions(x, function_name):
     if function_name == "sigmoid":
-        sigmoid = 1/(1 + math.exp(-x))
-        return f"sigmoid: f({x})={sigmoid}"
+        sigmoid = 1 / (1 + math.exp(-x))
+        return f"sigmoid: f({x}) = {sigmoid}"
 
     elif function_name == "relu":
         if x<=0:
-            return f"relu: f({x})={0}"
+            return f"relu: f({x}) = {0}"
         else:
-            return f"relu: f({x})={x}"
+            return f"relu: f({x}) = {x}"
 
     elif function_name == "elu":
         if x<=0:
             alpha = 0.001
-            elu = alpha*(math.exp(x) - 1)
-            return f"elu: f({x})={elu}"
+            elu = alpha * (math.exp(x) - 1)
+            return f"elu: f({x}) = {elu}"
         else:
-            return f"elu: f({x})={x}"
+            return f"elu: f({x}) = {x}"
 
     else:
         return f"{function_name} is not supported"
 
 def is_number(x):
-    check_x = type(x)
-
-    if check_x is (int or float):
-        return True
-    else:
-        return False
+    return isinstance(x, (int, float))
     
 if __name__ == "__main__":
     x = -5
--- week1/ex5_mean_difference.py
+++ week1/ex5_mean_difference.py
@@ -1,6 +1,6 @@
 # MD_nRE 
 def md_nre_single_sample(y, y_hat, n, p):
-    Loss = (y**(1/n) - y_hat**(1/n))**p
+    Loss = (y ** (1 / n) - y_hat ** (1 / n)) ** p
     return Loss
 
 if __name__ == "__main__":
