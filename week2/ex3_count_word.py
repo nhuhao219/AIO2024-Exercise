1422324de09b1c778d22022d8bcec048e0e9a766
HaoGau <nguyennhuhao219@gmail.com>
2024-06-12 10:20:08 +0700

    exercise week2

---
+++ week2/ex3_count_word.py
@@ -0,0 +1,16 @@
+def count_word(file_path):
+    dic = {}
+    with open(file_path, "r") as file:
+        data = file.read()
+    words = data.split()
+    for word in words:
+        if word in dic:
+            dic[word] += 1
+        else:
+            dic[word] = 1
+    return dic
+
+
+if __name__ == "__main__":
+    file_path = "data.txt"
+    print(count_word(file_path))
